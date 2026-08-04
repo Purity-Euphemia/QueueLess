64434562d9139235f2d95f897cb8ad2f7cb7a565
cobachie <euphemiaobachie@gmail.com>
2026-08-04 12:29:37 +0100

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,35 @@
 from flask import Flask
 
+from database import create_tables
+from routes.queue_routes import queue_routes
+from routes.admin_routes import admin_routes
+
+
+# Create the Flask application
 app = Flask(__name__)
 
 
+# Register the user queue routes
+app.register_blueprint(queue_routes)
+
+
+# Register the admin routes
+app.register_blueprint(admin_routes)
+
+
+# Home route
 @app.route("/")
 def home():
     return {
-        "message": "Welcome to QueueLess",
+        "message": "Welcome to QueueLess!",
         "status": "Backend is running"
     }
 
 
 if __name__ == "__main__":
+
+    # Create the database tables
+    create_tables()
+
+    # Start the Flask server
     app.run(debug=True)
