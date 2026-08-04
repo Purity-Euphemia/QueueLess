e0e8a833763d79c20cfb977f290657f86615163e
cobachie <euphemiaobachie@gmail.com>
2026-08-04 12:30:40 +0100

    first commit

--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,23 @@
-class QueueUser:
-    def __init__(self, name, position):
+class Queue:
+
+    def __init__(self, id, name, status):
+
+        self.id = id
         self.name = name
-        self.position = position
+        self.status = status
+
+
+class QueueMember:
+
+    def __init__(
+        self,
+        id,
+        queue_id,
+        name,
+        status
+    ):
+
+        self.id = id
+        self.queue_id = queue_id
+        self.name = name
+        self.status = status
